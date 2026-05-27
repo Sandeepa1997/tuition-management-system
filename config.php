9ea4dbabf8aac1dc67e74544e6d271fa4c69cb8d
Prashan <prashankannangara@gmail.com>
2026-05-27 14:49:48 +0530

    Updated  config.php

--- config.php
+++ config.php
@@ -1,6 +1,9 @@
 <?php
-define("WEB_URL","http://localhost/Sciencemore/web/");
-define("SYS_URL","http://localhost/Sciencemore/system/");
+require __DIR__ . '/vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
+define("WEB_URL", $_ENV['WEB_URL']);
+define("SYS_URL", $_ENV['SYS_URL']);
 ?>
